48b5719da49b278fceffc839166bcee7da3886a2
Jason Thomo <jthomo@uvic.ca>
2024-01-12 23:42:47 -0800

    misc

--- download.py
+++ download.py
@@ -2,33 +2,34 @@ import yfinance as yf
 import pandas as pd
 
 # tickers = pd.read_csv('data/tickers_small_plus.csv', squeeze=True).dropna().to_list()
-tickers = [
-    'AGL',
-    'MPW',
-    'VYGR',
-    'ALLO',
-    'CPRX',
-    'TBPH',
-    'PUYI',
-    'DYN',
-    'AMBI',
-    'DH',
-    'BLND',
-    'BGNE',
-    'EXAI',
-    'BALY',
-    'IMCR',
-    'ZH',
-    'BFST',
-    'LBPH',
-    'ERAS',
-    'VMEO',
-]
+tickers = pd.read_csv('data/tickers_large_plus.csv', squeeze=True).dropna().to_list()
+# tickers = [
+#     'AGL',
+#     'MPW',
+#     'VYGR',
+#     'ALLO',
+#     'CPRX',
+#     'TBPH',
+#     'PUYI',
+#     'DYN',
+#     'AMBI',
+#     'DH',
+#     'BLND',
+#     'BGNE',
+#     'EXAI',
+#     'BALY',
+#     'IMCR',
+#     'ZH',
+#     'BFST',
+#     'LBPH',
+#     'ERAS',
+#     'VMEO',
+# ]
 
-df = yf.download(tickers, '2024-01-04', '2024-01-06', auto_adjust=True)
-df.to_csv('data/temp.csv')
+df = yf.download(tickers, '2020-01-01', '2024-01-01', auto_adjust=True)
+# df.to_csv('data/temp.csv')
 
 # df = pd.read_csv('data/temp.csv', header=[0, 1], index_col=0, parse_dates=True)
 
 for attribute in df.columns.levels[0]:
-    df[attribute].to_csv(f'data/temp/{attribute}.csv')
+    df[attribute].to_csv(f'data/data_2020-2024_large/{attribute}.csv')
--- test.py
+++ test.py
@@ -3,42 +3,45 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-open_df = pd.read_csv('data/data_2020-2024/Open.csv', index_col=0, parse_dates=True)
-close_df = pd.read_csv('data/data_2020-2024/Close.csv', index_col=0, parse_dates=True)
+INPUT_DATA_DIR = 'data/data_2020-2024_large'
+N_STOCKS = 5
 
-# Shift the close_prices DataFrame to align with the next day's open prices
-previous_close = close_df.shift(1)
+open_df = pd.read_csv(f'{INPUT_DATA_DIR}/Open.csv', index_col=0, parse_dates=True)
+close_df = pd.read_csv(f'{INPUT_DATA_DIR}/Close.csv', index_col=0, parse_dates=True)
 
-gap_down = (previous_close - open_df) / previous_close
+# Shift close prices down to align with the next day's open prices
+prev_close = close_df.shift(1)
 
-n_stocks = 15
-top_gap_down_stocks = gap_down.apply(lambda x: x.nlargest(n_stocks).index, axis=1)
-top_gap_up_stocks = gap_down.apply(lambda x: x.nsmallest(n_stocks).index, axis=1)
+# Calculate gap between prev close and next day's open for each stock
+gap_down = (prev_close - open_df) / prev_close
 
+top_gap_down_stocks = gap_down.apply(lambda x: x.nlargest(N_STOCKS).index, axis=1)
+top_gap_up_stocks = gap_down.apply(lambda x: x.nsmallest(N_STOCKS).index, axis=1)
 
-
-pct_changes_1 = pd.DataFrame(index=top_gap_down_stocks.index)
+# Calculate the percentage change from open to close for each top-n stock
+pct_changes_1 = pd.DataFrame(index=top_gap_down_stocks.index)  # long gap down
 for date, stocks in top_gap_down_stocks.iteritems():
     for stock in stocks:
         pct_changes_1.at[date, stock] = 100 * (close_df.at[date, stock] - open_df.at[date, stock]) / open_df.at[date, stock]
 
-pct_changes_2 = pd.DataFrame(index=top_gap_up_stocks.index)
+pct_changes_2 = pd.DataFrame(index=top_gap_up_stocks.index)  # short gap up
 for date, stocks in top_gap_up_stocks.iteritems():
     for stock in stocks:
         pct_changes_2.at[date, stock] = -100 * (close_df.at[date, stock] - open_df.at[date, stock]) / open_df.at[date, stock]
 
+# Combine long (1) and short (2)
 pct_change = pct_changes_1.add(pct_changes_2, fill_value=0)
 
-# Calculate the average percentage change for each day
-avg_pct_change_1 = pct_changes_1.mean(axis=1)
-avg_pct_change_2 = pct_changes_2.mean(axis=1)
-avg_pct_change = pct_change.mean(axis=1)
+# Calculate the avg % change across all stocks for each day
+daily_pct_change_1 = pct_changes_1.mean(axis=1)
+daily_pct_change_2 = pct_changes_2.mean(axis=1)
+daily_pct_change = pct_change.mean(axis=1)
 
-# savw the average percentage change for each day to a CSV file
-avg_pct_change_1.to_csv(f'data/avg_pct_change_1_{n_stocks}.csv')
-avg_pct_change_2.to_csv(f'data/avg_pct_change_2_{n_stocks}.csv')
-avg_pct_change.to_csv(f'data/avg_pct_change_{n_stocks}.csv')
+daily_pct_change_1.to_csv(f'data/daily_pct_change_1_large_{N_STOCKS}.csv')
+daily_pct_change_2.to_csv(f'data/daily_pct_change_2_large_{N_STOCKS}.csv')
+daily_pct_change.to_csv(f'data/daily_pct_change_large_{N_STOCKS}.csv')
 
-print(avg_pct_change_1.mean(), avg_pct_change_1.std(), avg_pct_change_1.max(), avg_pct_change_1.min())
-print(avg_pct_change_2.mean(), avg_pct_change_2.std(), avg_pct_change_2.max(), avg_pct_change_2.min())
-print(avg_pct_change.mean(), avg_pct_change.std(), avg_pct_change.max(), avg_pct_change.min())
+# Statistics
+print(daily_pct_change_1.mean(), daily_pct_change_1.std(), daily_pct_change_1.max(), daily_pct_change_1.min())
+print(daily_pct_change_2.mean(), daily_pct_change_2.std(), daily_pct_change_2.max(), daily_pct_change_2.min())
+print(daily_pct_change.mean(), daily_pct_change.std(), daily_pct_change.max(), daily_pct_change.min())
